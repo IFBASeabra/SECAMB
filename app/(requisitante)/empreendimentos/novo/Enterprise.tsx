e65403512592ac53d4bb96bcbe7d033f99a02cc1
Giovana Rodrigues <giovana.rodrigues1112@gmail.com>
2025-02-27 09:54:00 -0300

    feat: integração dos dados de empreendimento com o banco de dados

--- app/(requisitante)/empreendimentos/novo/Enterprise.tsx
+++ app/(requisitante)/empreendimentos/novo/Enterprise.tsx
@@ -1,153 +1,210 @@
-"use client";
+'use client';
 
-import React, { useState } from "react";
-import { zodResolver } from "@hookform/resolvers/zod";
-import { useForm } from "react-hook-form";
-import Link from "next/link";
-import { z } from "zod";
+import React, { useState } from 'react';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useForm } from 'react-hook-form';
+import Link from 'next/link';
+import { z } from 'zod';
 
+import { EnterpriseActionState } from '@/app/actions/enterprise';
+import { enterpriseFormSchema } from '@/schemas/enterprise';
 
-import { EnterpriseActionState } from "@/app/actions/enterprise";
-import { enterpriseFormSchema } from "@/schemas/enterprise";
+import { FormMessage, Message } from '@/components/form-message';
+import { Input } from '@/components/ui/input';
+import { Label } from '@/components/ui/label';
 
-
-import { FormMessage, Message } from "@/components/form-message";
-import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
-
-import "./enterprise.scss";
-
+import './enterprise.scss';
 
 type EnterpriseFormType = {
   searchParams: Message;
   enterpriseAction: (formData: FormData) => Promise<EnterpriseActionState>;
 };
 
-const EnterpriseForm = ({ searchParams, enterpriseAction }: EnterpriseFormType) => {
+const EnterpriseForm = ({
+  searchParams,
+  enterpriseAction,
+}: EnterpriseFormType) => {
   const form = useForm<z.output<typeof enterpriseFormSchema>>({
     resolver: zodResolver(enterpriseFormSchema),
   });
 
-   console.log('form.formState.errors: ', form.formState.errors)
-    
-    const registerUser = async (data: z.output<typeof enterpriseFormSchema>) => {
-      const formData = new FormData();
-  
-      formData.append('name', data.name)
-      formData.append('cnpj', data.cnpj)
-      formData.append('activity_type', data.activity_type)
-      formData.append('address', data.address)
-      formData.append('neighborhoode', data.neighborhood)
-      formData.append('zipcode', data.zipcode)
-      formData.append('telephone_contact', data.telephone_contact)
-      formData.append('cellphone_contact', data.cellphone_contact)
-      formData.append('email', data.email)
-      formData.append('uc', data.uc)
-      formData.append('river_basin', data.river_basin)
-      formData.append('water_resource', data.water_resource)
-      formData.append('geographic_coordinates', data.geographic_coordinates)
-      formData.append('operation_phase', data.operation_phase)
+  const registerEmpreendimento = async (
+    data: z.output<typeof enterpriseFormSchema>,
+  ) => {
+    const formData = new FormData();
+    console.log('form.formState.errors: ', form.formState.errors);
 
-      await enterpriseAction(formData)
-    }
+    formData.append('name', data.name);
+    formData.append('cnpj', data.cnpj);
+    // formData.append('activity_type', data.activity_type)
+    formData.append('address', data.address);
+    formData.append('neighborhood', data.neighborhood);
+    formData.append('zipcode', data.zipcode);
+    formData.append('contact_name', data.contact_name);
+    formData.append('telephone_contact', data.telephone_contact ?? '');
+    formData.append('cellphone_contact', data.cellphone_contact ?? '');
+    formData.append('email', data.email);
+    //   formData.append('uc', data.uc)
+    formData.append('river_basin', data.river_basin);
+    formData.append('water_resource', data.water_resource);
+    //  formData.append('geographic_coordinates', data.geographic_coordinates)
+    formData.append('operation_phase', data.operation_phase);
 
+    await enterpriseAction(formData);
+  };
 
-    return (
-      <form
-        className="register"
-        onSubmit={form.handleSubmit(registerUser)}
-      >
-        <h1 className="register__title">Empreendimento</h1>
-        <div className="register__form" >
+  return (
+    <form
+      className="register"
+      onSubmit={form.handleSubmit(registerEmpreendimento)}
+    >
+      <h1 className="register__title">Empreendimento</h1>
+      <div className="register__form">
+        <div className="register__form-group">
+          <Label htmlFor="name">Nome do Empreendimento</Label>
+          <Input
+            {...form.register('name')}
+            name="name"
+            placeholder="Casas Bahia"
+            required
+          />
+        </div>
+
+        <div className="register__form-group">
+          <Label htmlFor="cnpj">CNPJ</Label>
+          <Input
+            {...form.register('cnpj')}
+            name="cnpj"
+            placeholder="00.000.000/0001-00"
+            required
+          />
+
+          {form.formState.errors.cnpj && (
+            <p className="form-error">{form.formState.errors.cnpj.message}</p>
+          )}
+        </div>
+
+        <div className="register__form-group">
+          <Label htmlFor="address">Endereço</Label>
+          <Input
+            {...form.register('address')}
+            name="address"
+            placeholder="Rua Horáco de Matos"
+            required
+          />
+
+          {form.formState.errors.address && (
+            <p className="form-error">
+              {form.formState.errors.address.message}
+            </p>
+          )}
+        </div>
+
+        <div className="register__form-group">
+          <Label htmlFor="neighborhood">Bairro</Label>
+          <Input
+            {...form.register('neighborhood')}
+            name="neighborhood"
+            placeholder="São José"
+            required
+          />
+
+          {form.formState.errors.neighborhood && (
+            <p className="form-error">
+              {form.formState.errors.neighborhood.message}
+            </p>
+          )}
+        </div>
+
+        <div className="register__form-group">
+          <Label htmlFor="zipcode">CEP</Label>
+          <Input
+            {...form.register('zipcode')}
+            name="zipcode"
+            placeholder="46900-000"
+            required
+          />
+          {form.formState.errors.zipcode && (
+            <p className="form-error">
+              {form.formState.errors.zipcode.message}
+            </p>
+          )}
+        </div>
+
+        <div className="register__form-group">
+          <Label htmlFor="river_basin">Bacia Hidrográfica:</Label>
+          <Input
+            {...form.register('river_basin')}
+            name="river_basin"
+            placeholder="Bacia X"
+            required
+          />
+          {form.formState.errors.river_basin && (
+            <p className="form-error">
+              {form.formState.errors.river_basin.message}
+            </p>
+          )}
+        </div>
+
+        <div className="register__form-group">
+          <Label htmlFor="water_resource">Recurso Hídrico:</Label>
+          <Input
+            {...form.register('water_resource')}
+            name="water_resource"
+            placeholder="Rio Y"
+            required
+          />
+          {form.formState.errors.water_resource && (
+            <p className="form-error">
+              {form.formState.errors.water_resource.message}
+            </p>
+          )}
+        </div>
+
+        <div className="register__form-group">
+          <Label htmlFor="operation_phase">Fase Atual do Empreendimento:</Label>
+          <select
+            {...form.register('operation_phase')}
+            name="operation_phase"
+            className="register__select"
+            required
+          >
+            <option value="">Selecione uma opção</option>
+            <option value="Localização">Localização</option>
+            <option value="Instalação">Instalação</option>
+            <option value="Operação">Operação</option>
+            <option value="Não se Aplica">Não se Aplica</option>
+          </select>
+          {form.formState.errors.operation_phase && (
+            <p className="form-error">
+              {form.formState.errors.operation_phase.message}
+            </p>
+          )}
+        </div>
+
+        <div className="flex flex-col gap-8">
+          <h2 className="my-4 text-xl">Dados de Contato</h2>
+
           <div className="register__form-group">
-            <Label htmlFor="name">Nome do Empreendimento</Label>
+            <Label htmlFor="contact_name">Nome do contato:</Label>
             <Input
-              {...form.register("name")}
-              name="name"
-              placeholder="Casas Bahia"
+              {...form.register('contact_name')}
+              name="contact_name"
+              placeholder="José dos Santos"
               required
             />
-          </div>
-
-          <div className="register__form-group">
-            <Label htmlFor="cnpj">CNPJ</Label>
-            <Input
-              {...form.register("cnpj")}
-              name="cnpj"
-              placeholder="00.000.000/0001-00"
-              required
-            />
-  
-            {form.formState.errors.cnpj && (
+            {form.formState.errors.telephone_contact && (
               <p className="form-error">
-                {form.formState.errors.cnpj.message}
+                {form.formState.errors.telephone_contact.message}
               </p>
             )}
           </div>
-
-
-          //tipo de atividade(pesquisar)
-
-  
-          <div className="register__form-group">
-            <Label htmlFor="address">Endereço</Label>
-            <Input
-              {...form.register("address")}
-              name="address"
-              placeholder="Rua Horáco de Matos"
-              required
-            />
-  
-            {form.formState.errors.address && (
-              <p className="form-error">
-                {form.formState.errors.address.message}
-              </p>
-            )}
-          </div>
-
-
-
-          <div className="register__form-group">
-            <Label htmlFor="neighborhood">Bairro</Label>
-            <Input
-              {...form.register("neighborhood")}
-              name="neighborhood"
-              placeholder="Bloco"
-              required
-            />
-  
-            {form.formState.errors.neighborhood && (
-              <p className="form-error">
-                {form.formState.errors.neighborhood.message}
-              </p>
-            )}
-          </div>
-
-
-  
-          <div className="register__form-group">
-            <Label htmlFor="zipcode">CEP</Label>
-            <Input
-              {...form.register("zipcode")}
-              name="zipcode"
-              placeholder="xxxxx-xxx"
-              required
-            />
-            {form.formState.errors.zipcode && (
-              <p className="form-error">
-                {form.formState.errors.zipcode.message}
-              </p>
-            )}
-          </div>
-
-
-
           <div className="register__form-group">
             <Label htmlFor="telephone_contact">Telefone:</Label>
             <Input
-              {...form.register("telephone_contact")}
+              {...form.register('telephone_contact')}
               name="telephone_contact"
-              placeholder="(75)99999-9999"
+              placeholder="(75) 99999-9999"
               required
             />
             {form.formState.errors.telephone_contact && (
@@ -157,14 +214,12 @@ const EnterpriseForm = ({ searchParams, enterpriseAction }: EnterpriseFormType) 
             )}
           </div>
 
-
-
           <div className="register__form-group">
             <Label htmlFor="cellphone_contact">Celular:</Label>
             <Input
-              {...form.register("cellphone_contact")}
+              {...form.register('cellphone_contact')}
               name="cellphone_contact"
-              placeholder="(75)99999-9999"
+              placeholder="(75) 99999-9999"
               required
             />
             {form.formState.errors.cellphone_contact && (
@@ -174,12 +229,10 @@ const EnterpriseForm = ({ searchParams, enterpriseAction }: EnterpriseFormType) 
             )}
           </div>
 
-
-
           <div className="register__form-group">
             <Label htmlFor="email">E-mail:</Label>
             <Input
-              {...form.register("email")}
+              {...form.register('email')}
               name="email"
               placeholder="jose@gmail.com"
               required
@@ -190,59 +243,15 @@ const EnterpriseForm = ({ searchParams, enterpriseAction }: EnterpriseFormType) 
               </p>
             )}
           </div>
-
-
-            // uc
-            // uc
-
-
-
-          <div className="register__form-group">
-            <Label htmlFor="river_basin">Bacia Hidrográfica:</Label>
-            <Input
-              {...form.register("river_basin")}
-              name="river_basin"
-              placeholder="Bacia X"
-              required
-            />
-            {form.formState.errors.river_basin && (
-              <p className="form-error">
-                {form.formState.errors.river_basin.message}
-              </p>
-            )}
-          </div>  
-
-
-
-          <div className="register__form-group">
-            <Label htmlFor="water_resource">Recurso Hídrico:</Label>
-            <Input
-              {...form.register("water_resource")}
-              name="water_resource"
-              placeholder="Rio Y"
-              required
-            />
-            {form.formState.errors.water_resource && (
-              <p className="form-error">
-                {form.formState.errors.water_resource.message}
-              </p>
-            )}
-          </div> 
-
-
-
-          // Coordenadas geográficas
-          // Fase atual do empreendimento
-
-
-          <button type="submit" className="register__submit">
-            Cadastrar
-          </button>
         </div>
-        <FormMessage message={searchParams} />
-      </form>
-    );
-  };
-  
-  export default EnterpriseForm;
-  
+
+        <button type="submit" className="register__submit">
+          Cadastrar
+        </button>
+      </div>
+      <FormMessage message={searchParams} />
+    </form>
+  );
+};
+
+export default EnterpriseForm;
--- schemas/enterprise.ts
+++ schemas/enterprise.ts
@@ -1,62 +1,82 @@
-import { z } from "zod";
+import { z } from 'zod';
 
 export const enterpriseFormSchema = z.object({
-	name: z
-		.string()
-		.regex(/^[A-Za-zÀ-ÖØ-öø-ÿ]+(?: [A-Za-zÀ-ÖØ-öø-ÿ]+)+$/, {
-			message:
-				"Informe o nome completo da sua empresa, sem caracteres especiais",
-		})
-		.trim(),
+  name: z
+    .string()
+    .regex(/^[A-Za-zÀ-ÖØ-öø-ÿ]+(?: [A-Za-zÀ-ÖØ-öø-ÿ]+)+$/, {
+      message:
+        'Informe o nome completo da sua empresa, sem caracteres especiais',
+    })
+    .trim(),
 
-	cnpj: z.string(),
+  cnpj: z.string(),
 
-	activity_type: z.enum([
-		"POR AQUI NADA ",
-		"POR NADA AQ AINDA",
-		"POR AQUI AINDA NADA ",
-	]),
+  //activity_type: z.enum([
+  //"POR AQUI NADA ",
+  //"POR NADA AQ AINDA",
+  //"POR AQUI AINDA NADA ",
+  //]),
 
-	address: z
-		.string()
-		.min(5, "O endereço deve ter pelo menos 5 caracteres")
-		.trim(),
+  address: z
+    .string()
+    .min(5, 'O endereço deve ter pelo menos 5 caracteres')
+    .trim(),
 
-	neighborhood: z
-		.string()
-		.min(3, "O bairro deve ter pelo menos 3 caracteres")
-		.trim(),
+  neighborhood: z
+    .string()
+    .min(3, 'O bairro deve ter pelo menos 3 caracteres')
+    .trim(),
 
-	zipcode: z
-		.string()
-		.regex(/^\d{5}-\d{3}$/, {
-			message: "CEP inválido. Use o formato XXXXX-XXX.",
-		})
-		.trim(),
+  zipcode: z
+    .string()
+    .regex(/^\d{5}-\d{3}$/, {
+      message: 'CEP inválido. Use o formato XXXXX-XXX.',
+    })
+    .trim(),
 
-	telephone_contact: z
-		.string()
-		.regex(/^\(\d{2}\) \d{4}-\d{4}$/, "Telefone inválido")
-		.trim(),
+  contact_name: z
+    .string()
+    .regex(/^[A-Za-zÀ-ÖØ-öø-ÿ]+(?: [A-Za-zÀ-ÖØ-öø-ÿ]+)+$/, {
+      message: 'Informe o nome completo do contato, sem caracteres especiais',
+    })
+    .trim(),
 
-	cellphone_contact: z
-		.string()
-		.regex(/^\(\d{2}\) \d{5}-\d{4}$/, "Celular inválido")
-		.trim(),
+  telephone_contact: z
+    .string()
+    .regex(
+      /^\(\d{2}\) (?:\d{4,5}-\d{4})$/,
+      'Telefone inválido. Use o formato (XX) XXXX-XXXX ou (XX) 9XXXX-XXXX.',
+    )
+    .optional()
+    .or(z.string().length(0)), // Permite string vazia ao invés de " "
 
-	email: z.string().email({ message: "Informe um e-mail válido." }).trim(),
+  cellphone_contact: z
+    .string()
+    .regex(
+      /^\(\d{2}\) (?:\d{4,5}-\d{4})$/,
+      'Telefone inválido. Use o formato (XX) XXXX-XXXX ou (XX) 9XXXX-XXXX.',
+    )
+    .optional()
+    .or(z.string().length(0)), // Permite string vazia ao invés de " "
 
-	uc: z.enum(["sim", "nao"]),
+  email: z.string().email({ message: 'Informe um e-mail válido.' }).trim(),
 
-	river_basin: z.string().trim(),
+  //uc: z.enum(["sim", "nao"]),
 
-	water_resource: z.string().trim(),
+  river_basin: z.string().trim(),
 
-	geographic_coordinates: z.string().trim(),
+  water_resource: z.string().trim(),
 
-	longitude: z.string().min(1, "Longitude obrigatória").trim(),
+  //geographic_coordinates: z.string().trim(),
 
-	latitude: z.string().min(1, "Latitude obrigatória").trim(),
+  //longitude: z.string().min(1, "Longitude obrigatória").trim(),
 
-	operation_phase: z.string().trim(),
+  //latitude: z.string().min(1, "Latitude obrigatória").trim(),
+
+  operation_phase: z.enum(
+    ['Localização', 'Instalação', 'Operação', 'Não se Aplica'],
+    {
+      errorMap: () => ({ message: 'Selecione uma fase válida' }),
+    },
+  ),
 });
