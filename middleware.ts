2b8c90ef9fef489173bf5e528e4f793683e498eb
Giovana Rodrigues <giovana.rodrigues1112@gmail.com>
2025-04-23 14:34:09 -0300

    feat: atualizacao da rota do editor no middleware

--- middleware.ts
+++ middleware.ts
@@ -1,10 +1,10 @@
-import { NextResponse, type NextRequest } from "next/server";
-import { updateSession } from "@/utils/supabase/middleware";
-import { createClient } from "./utils/supabase/server";
-import { cookies } from "next/headers";
+import { NextResponse, type NextRequest } from 'next/server';
+import { updateSession } from '@/utils/supabase/middleware';
+import { createClient } from './utils/supabase/server';
+import { cookies } from 'next/headers';
 
 export async function middleware(request: NextRequest) {
-  if (request.nextUrl.pathname === "/") {
+  if (request.nextUrl.pathname === '/') {
     const supabase = await createClient();
     const cookieStore = await cookies();
 
@@ -12,40 +12,47 @@ export async function middleware(request: NextRequest) {
       data: { user },
     } = await supabase.auth.getUser();
 
-    console.log("user:", user)
-  
+    console.log('user:', user);
+
     if (!user) {
-      cookieStore.delete('user')
+      cookieStore.delete('user');
 
       return NextResponse.redirect(new URL('/sign-in', request.url));
     }
 
-    const {data, error} = await supabase.from("user_info").select().eq("user_id", user.id).single();
+    const { data, error } = await supabase
+      .from('user_info')
+      .select()
+      .eq('user_id', user.id)
+      .single();
 
-    console.log('data: ', data)
+    console.log('data: ', data);
 
     if (error) {
-      console.error("error: ", error)
-      return NextResponse.redirect(new URL('/unauthorized', request.url))
-    } 
+      console.error('error: ', error);
+      return NextResponse.redirect(new URL('/unauthorized', request.url));
+    }
 
-    const { user_id, name, profile } = data
+    const { user_id, name, profile } = data;
 
-    console.log(`${profile} === "admin": ${profile === "admin"}`)
-    console.log(`${profile} === "user": ${profile === "user"}`)
+    console.log(`${profile} === "editor": ${profile === 'editor'}`);
+    console.log(`${profile} === "user": ${profile === 'user'}`);
 
-      cookieStore.set('user', JSON.stringify({id: user_id, name, profile}), {secure: true, sameSite: 'strict'})
-    
-      if (profile === "admin") {
-        return NextResponse.redirect(new URL("/admin", request.url));
-      }
+    cookieStore.set('user', JSON.stringify({ id: user_id, name, profile }), {
+      secure: true,
+      sameSite: 'strict',
+    });
 
-      if (profile === "user") {
-        return NextResponse.redirect(new URL("/home", request.url));
-      }
+    if (profile === 'editor') {
+      return NextResponse.redirect(new URL('/secretaria', request.url));
+    }
 
-    return NextResponse.redirect(new URL('/unauthorized', request.url))
-  } 
+    if (profile === 'user') {
+      return NextResponse.redirect(new URL('/home', request.url));
+    }
+
+    return NextResponse.redirect(new URL('/unauthorized', request.url));
+  }
 
   return await updateSession(request);
 }
@@ -60,6 +67,6 @@ export const config = {
      * - images - .svg, .png, .jpg, .jpeg, .gif, .webp
      * Feel free to modify this pattern to include more paths.
      */
-    "/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)",
+    '/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)',
   ],
 };
